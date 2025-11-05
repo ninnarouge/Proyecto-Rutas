ec76648e386f47397feaec712d2a7699a5184a93
PAAWSA02LC0909 <Duoc@PAAWSA02LC0909.duoc.acad>
2025-11-04 22:07:50 -0300

    Se agrega pagina productos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePrincipal from './pages/home';
+import ProductosPrincipal from './pages/products';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<HomePrincipal/>}></Route>
+        <Route path='/products' element={<ProductosPrincipal/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/products.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import NavbarPrincipal from './components/navbar';
+
+
+function ProductosPrincipal() {
+    return (
+        <div>
+            <NavbarPrincipal />
+            <div className='row mt-4'>
+                <div className='col-md-12'>
+                    <h2>Pagina Prouctos</h2>
+                    <p> bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla</p>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    )
+}
+
+export default ProductosPrincipal;
