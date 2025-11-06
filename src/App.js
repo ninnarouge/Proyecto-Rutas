eb51d18034fc3a433498e3821ffbdc55d9c08a01
PAAWSA09LC0910 <Duoc@PAAWSA09LC0910.duoc.acad>
2025-11-05 22:14:26 -0300

    se actualiza

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePrincipal from './pages/home';
 import ProductosPrincipal from './pages/products';
+import ListaProducto from './pages/product_list';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePrincipal/>}></Route>
         <Route path='/products' element={<ProductosPrincipal/>}></Route>
+        <Route path='/lista-productos' element={<ListaProducto/>}></Route>
       </Routes>
     </Router>
   );
